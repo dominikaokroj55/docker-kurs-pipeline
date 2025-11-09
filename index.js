a9b6690501d2b3ff607f54843121cae90f02ad45
dominikaokroj55 <dominika.okroj55@wp.pl>
2025-11-09 18:14:48 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('wszystkie nerwy juz na tym stracilam!'));
+app.listen(3000, () => console.log('Server running on 3000'));
